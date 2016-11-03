b9996efa2496ab92cdf34c42633c913fc0817698
cetinekr <e.cihadcetin@gmail.com>
2016-11-03 12:47:08 +0200

    Hataya yol açtıgından dolayı, takım arkadası tarafından yapılan bazı kodlamalar kaldırıldı.

--- initialize_db.py
+++ initialize_db.py
@@ -1,35 +1,37 @@
 def initialize_db_func(cursor):
-    
+
     #Database for counter
     cursor.execute("""DROP TABLE IF EXISTS COUNTER""")
     cursor.execute("""CREATE TABLE COUNTER (N INTEGER)""")
     cursor.execute("""INSERT INTO COUNTER (N) VALUES (0)""")
-    
-    #Table for users
-    cursor.execute("""DROP TABLE IF EXISTS USER""")
-    cursor.execute("""CREATE TABLE USER (USERNAME CHAR(20) UNIQUE PRIMARY KEY NOT NULL, PASSWORD CHAR(20) )""")
 
-    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('user','pass')""")
-    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('aliercccan','pass')""")
-    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('dincer','pass')""")
-    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('fatih','pass')""")
-    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('caglar','pass')""")
-    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('ekrem','pass')""")
+ #==============================================================================
+ #    #Table for users
+ #    cursor.execute("""DROP TABLE IF EXISTS USER""")
+ #    cursor.execute("""CREATE TABLE USER (USERNAME CHAR(20) UNIQUE PRIMARY KEY NOT NULL, PASSWORD CHAR(20) )""")
+ #
+ #    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('user','pass')""")
+ #    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('aliercccan','pass')""")
+ #    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('dincer','pass')""")
+ #    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('fatih','pass')""")
+ #    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('caglar','pass')""")
+ #    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('ekrem','pass')""")
+ #
+ #    #Table for admins
+ #    cursor.execute("""DROP TABLE IF EXISTS ADMIN""")
+ #    cursor.execute("""CREATE TABLE ADMIN (USERNAME CHAR(20) UNIQUE PRIMARY KEY NOT NULL, Password CHAR(20) )""")
+ #
+ #    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('admin','admin')""")
+ #    cursor.execute("""INSERT INTO ADMIN (USERNAME, PASSWORD) VALUES ('aliercccan','admin')""")
+ #    cursor.execute("""INSERT INTO ADMIN (USERNAME, PASSWORD) VALUES ('fatih','admin')""")
+ #==============================================================================
 
-    #Table for admins
-    cursor.execute("""DROP TABLE IF EXISTS ADMIN""")
-    cursor.execute("""CREATE TABLE ADMIN (USERNAME CHAR(20) UNIQUE PRIMARY KEY NOT NULL, Password CHAR(20) )""")
-
-    cursor.execute("""INSERT INTO USER (USERNAME, PASSWORD) VALUES ('admin','admin')""")
-    cursor.execute("""INSERT INTO ADMIN (USERNAME, PASSWORD) VALUES ('aliercccan','admin')""")
-    cursor.execute("""INSERT INTO ADMIN (USERNAME, PASSWORD) VALUES ('fatih','admin')""")
-    
     #Table for main
     cursor.execute("""DROP TABLE IF EXISTS PostForView""")
     cursor.execute("""CREATE TABLE PostForView(ID CHAR(20) NOT NULL, FID CHAR(20), POSTID Char(10) )""")
     cursor.execute("""INSERT INTO PostForView (ID, FID, POSTID) VALUES ('2', '5', '1' )""")
     cursor.execute("""INSERT INTO PostForView (ID, FID, POSTID) VALUES ('2', '3', '4' )""")
-    
+
     #Table for explore
     cursor.execute("""DROP TABLE IF EXISTS hashTag""")
     cursor.execute("""CREATE TABLE hashTag (HASHID CHAR(20) NOT NULL, GRUPNAME CHAR(20) )""")
