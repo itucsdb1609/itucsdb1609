78e678b3edaa1c21228e79781632791d9b98a1bb
cetinekr <e.cihadcetin@gmail.com>
2016-11-29 08:28:42 +0200

    veritabanında user login ve signup icin foreign keylerle ekleme yapildi. Authentication işlemi su an icin gerçekleştirilmediğinden ilerleyen adımların devam etmesi icin gecici olarak bu tablolar, benim bölümüm olmadıgı halde olusturuldu.

--- initialize_db.py
+++ initialize_db.py
@@ -5,27 +5,54 @@ def initialize_db_func(cursor):
     cursor.execute("""CREATE TABLE COUNTER (N INTEGER)""")
     cursor.execute("""INSERT INTO COUNTER (N) VALUES (0)""")
 
+    #Table for GENDER
+    cursor.execute("""DROP TABLE IF EXISTS GENDER""")
+    cursor.execute("""CREATE TABLE GENDER (ID  SERIAL PRIMARY KEY,TYPE character varying(10) NOT NULL )""")
+    cursor.execute("""INSERT INTO GENDER ( TYPE) VALUES ('FEMALE' )""")
+    cursor.execute("""INSERT INTO GENDER ( TYPE) VALUES ( 'MALE' )""")
+    cursor.execute("""INSERT INTO GENDER ( TYPE) VALUES ( 'UNKNOWN' )""")
+    #Table for CITY
+    cursor.execute("""DROP TABLE IF EXISTS CITY""")
+    cursor.execute("""CREATE TABLE CITY (ID  SERIAL PRIMARY KEY,CITYNAME character varying(20) NOT NULL )""")
+    cursor.execute("""INSERT INTO CITY ( CITYNAME) VALUES ('Istanbul' )""")
+    cursor.execute("""INSERT INTO CITY ( CITYNAME) VALUES ( 'Ankara' )""")
+    cursor.execute("""INSERT INTO CITY ( CITYNAME) VALUES ( 'Adana' )""")
+     #Table for SCHOOL
+    cursor.execute("""DROP TABLE IF EXISTS SCHOOL""")
+    cursor.execute("""CREATE TABLE SCHOOL (ID  SERIAL PRIMARY KEY,SCHOOLNAME character varying(50) NOT NULL )""")
+    cursor.execute("""INSERT INTO SCHOOL ( SCHOOLNAME) VALUES ('ITU' )""")
+    cursor.execute("""INSERT INTO SCHOOL ( SCHOOLNAME) VALUES ( 'METU' )""")
+    cursor.execute("""INSERT INTO SCHOOL ( SCHOOLNAME) VALUES ( 'HACETTEPE UNIVERCITY' )""")
     #Table for user login
     cursor.execute("""DROP TABLE IF EXISTS USERLOGIN""")
     cursor.execute("""CREATE TABLE USERLOGIN (USERNAME VARCHAR(50) UNIQUE PRIMARY KEY NOT NULL,
-                                              PASSWORD VARCHAR(50) NOT NULL, 
+                                              PASSWORD VARCHAR(50) NOT NULL,
                                               LASTLOGIN VARCHAR(50))""")
 
-    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('ali','ali')""")
-    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('dincer','dincer')""")
-    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('fatih','fatih')""")
-    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('caglar','caglar')""")
-    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('ekrem','ekrem')""")
+    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('ali','password')""")
+    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('dincer','password')""")
+    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('fatih','password')""")
+    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('caglar','password')""")
+    cursor.execute("""INSERT INTO USERLOGIN (USERNAME, PASSWORD) VALUES ('ekrem','password')""")
 
     #Table for user information
     cursor.execute("""DROP TABLE IF EXISTS USERS""")
-    cursor.execute("""CREATE TABLE USERS (USERNAME VARCHAR(50) UNIQUE PRIMARY KEY NOT NULL,
+    cursor.execute("""CREATE TABLE USERS (USERNAME VARCHAR(50) PRIMARY KEY NOT NULL references USERLOGIN(USERNAME),
                                          NAME VARCHAR(50) NOT NULL,
                                          SURNAME VARCHAR(50) NOT NULL,
                                          MAIL VARCHAR(50) UNIQUE NOT NULL,
-                                         SEX INTEGER NOT NULL,
-                                         SCHOOL INTEGER NOT NULL,
-                                         CITY INTEGER NOT NULL)""")
+                                         GENDER INTEGER  references GENDER(ID),
+                                         SCHOOL INTEGER  references SCHOOL(ID),
+                                         CITY INTEGER  references CITY(ID))""")
+
+    cursor.execute("""INSERT INTO USERS (USERNAME, NAME, SURNAME, MAIL, GENDER, SCHOOL, CITY) VALUES ('ali',
+                                                                                                    'Ali',
+                                                                                                    'ERCAN',
+                                                                                                    'aliercan@gmail.com',
+                                                                                                    2,
+                                                                                                    1,
+                                                                                                    3)""")
+
 
     #Table for main
     cursor.execute("""DROP TABLE IF EXISTS PostForView""")
