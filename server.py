9bbd8ebcdb685741171a4227626d60a48c136525
Fatih Deniz <denizfa@itu.edu.tr>
2016-12-17 15:04:12 +0200

    Update on server.py
    Interest comments and likes added

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import psycopg2 as dbapi2
 import re
 import random
 from flask import Flask
+
 from flask import redirect
 from flask import render_template
 from werkzeug import redirect
@@ -12,6 +13,8 @@ from flask import request
 from flask.helpers import url_for, flash
 from initialize_db import initialize_db_func
 
+
+from models import *
 
 app = Flask(__name__)
 
@@ -83,27 +86,110 @@ def welcome_page():
 @app.route('/<user>', methods = ['GET','POST'])
 @app.route('/<user>/home', methods = ['GET','POST'])
 def home_page(user=None):
+    user_id=None
+    user_data=None
     posts = []
+    posts_likes={}
+    posts_comments={}
     allUsers = []
     userCheck = False
     with dbapi2.connect(app.config['dsn']) as connection:
+        post_like = PostLike(connection=connection)
+        post_comment = PostComments(connection=connection)
         cursor = connection.cursor()
-        query = """select id,username from users"""
+        query = """select id,username,name,surname from users"""
         cursor.execute(query)
         for currentUser in cursor:
             allUsers.append(currentUser)
 
-        for id,username in allUsers:
+        for uid,username,name,surname in allUsers:
             if user==username:
+                user_data=[username,name,surname]
+                user_id=uid
                 userCheck = True
                 break
 
-        if user and userCheck:
+        if request.method == 'POST':
+            if 'del' in request.form:
+                postid = request.form['postid']
+                userid = request.form['userid']
+                userName = request.form['username']
 
+                with dbapi2.connect(app.config['dsn']) as connection:
+                    cursor = connection.cursor()
+                    query = """INSERT INTO HIDDENPOSTS(userid, postid) VALUES (%s,%s) """
+                    cursor.execute(query, (userid, postid))
+                    connection.commit()
+                return redirect(url_for('home_page', user=userName))
+
+            elif 'upt' in request.form:
+                post = request.form['post']
+                desc = request.form['desc']
+                userName = request.form['username']
+
+                with dbapi2.connect(app.config['dsn']) as connection:
+                    cursor = connection.cursor()
+                    query = """UPDATE posts SET description='""" + desc + """' WHERE id =""" + post + """"""
+                    cursor.execute(query)
+
+                    connection.commit()
+                return redirect(url_for('home_page', user=userName))
+
+            elif 'like' in request.form:
+                post_id = request.form['postId']
+                like_type = request.form['like']
+                new_like = PostLike(post_id=post_id, user_id=user_id, like_type=like_type, connection=connection)
+                if not new_like.search_like_by_user_id_and_post_id():
+                    new_like.save()
+                else:
+                    new_like.update_like_type()
+
+            elif 'saveComment' in request.form:
+                post_id = request.form['postId']
+                comment =request.form['comment']
+                new_comment = PostComments(post_id=post_id, user_id=user_id, comment=comment, connection=connection)
+                saved_comment=new_comment.get_comment_by_user_id_and_post_id()
+                if saved_comment!=comment:
+                    new_comment.save()
+
+            elif 'unlike' in request.form:
+                like = PostLike(plid=request.form['unlike'], connection=connection)
+                like.delete()
+
+            elif 'uncomment' in request.form:
+                comment = PostComments(pcid=request.form['uncomment'], connection=connection)
+                comment.delete()
+            elif 'newInterest' in request.form:
+                new_interest=Interest(interest_name=request.form['interestName'],user_id=user_id,connection=connection)
+                new_interest.save()
+            elif 'deleteInterest' in request.form:
+                new_interest = Interest(iid=request.form['deleteInterest'], connection=connection)
+                new_interest.delete()
+            elif 'updateInterest' in request.form:
+                interest = Interest(iid=request.form['updateInterest'],connection=connection)
+                interest.update_interest(request.form['updatedName'])
+
+
+        if user and userCheck:
+            interest = Interest(connection=connection)
+            user_interests = interest.get_interest_by_user_id(user_id)
             query = """select m.userid, m.username, m.name, m.surname, m.linkpro, m.pid, m.linkpost, m.date, m.desc from (select users.id as userid, users.username as username, users.name as name, users.surname as surname, k.link1 as linkpro, k.id as pid, k.link2 as linkpost, k.date as date, k.description as desc from (select profilepic.link as link1,id,posts.userid as userid1,date,posts.link as link2,description from profilepic join posts on posts.userid=profilepic.userid) k join users on users.id=k.userid1) m left join hiddenposts on hiddenposts.postid=m.pid where hiddenposts.userid is null order by m.date desc"""
             cursor.execute(query)
+            temp = cursor.fetchall()
+            for post in temp:
+                if post[0]==user_id:
+                    user_data.append(post[4])
+                likes = post_like.get_likes_by_post_id(post[5])
+                if likes:
+                    posts_likes.update({post[5]: likes})
+                else:
+                    posts_likes.update({post[5]: False})
 
-            for post in cursor:
+                comments = post_comment.get_comments_by_post_id(post[5])
+                if comments:
+                    posts_comments.update({post[5]: comments})
+                else:
+                    posts_comments.update({post[5]: False})
                 posts.append(post)
 
         else:
@@ -116,33 +202,9 @@ def home_page(user=None):
 
             return redirect(url_for('home_page',user=userName))
 
-        if request.method =='POST':
-            if 'del' in request.form:
-                postid=request.form['postid']
-                userid=request.form['userid']
-                userName=request.form['username']
 
-                with dbapi2.connect(app.config['dsn']) as connection:
-                    cursor = connection.cursor()
-                    query = """INSERT INTO HIDDENPOSTS(userid, postid) VALUES (%s,%s) """
-                    cursor.execute(query,(userid,postid))
-                    connection.commit()
-                return redirect(url_for('home_page',user=userName))
 
-            if 'upt' in request.form:
-                post = request.form['post']
-                desc = request.form['desc']
-                userName=request.form['username']
-
-                with dbapi2.connect(app.config['dsn']) as connection:
-                    cursor = connection.cursor()
-                    query = """UPDATE posts SET description='"""+desc+"""' WHERE id ="""+post+""""""
-                    cursor.execute(query)
-
-                    connection.commit()
-                return redirect(url_for('home_page',user=userName))
-
-    return render_template('main.html', user=user, allUsers=allUsers , posts=posts)
+    return render_template('main.html', user=user, user_data=user_data, allUsers=allUsers , posts=posts,postsLikes=posts_likes,postsComments=posts_comments,user_interests=user_interests)
 
 @app.route('/add_post', methods = ['GET','POST'])
 def add_post():
@@ -364,32 +426,90 @@ def profile_page(user=None, user2=None):
     userr=[]
     allfollowerpic=[]
     allfollowingpic=[]
+    user_id=None
+    posts_likes = {}
+    posts_comments = {}
     if user==user2:
         user2=None
         return redirect(url_for('profile_page',user=user))
 
     with dbapi2.connect(app.config['dsn']) as connection:
+        post_like = PostLike(connection=connection)
+        post_comment = PostComments(connection=connection)
         cursor = connection.cursor()
         query = """select id,username from users"""
         cursor.execute(query)
         for im in cursor:
+            if im[1]==user:
+                user_id=im[0]
             kullanici.append(im)
+
+        if request.method == 'POST' and user_id:
+            if 'like' in request.form:
+                post_id = request.form['postId']
+                like_type = request.form['like']
+                new_like = PostLike(post_id=post_id, user_id=user_id, like_type=like_type, connection=connection)
+                if not new_like.search_like_by_user_id_and_post_id():
+                    new_like.save()
+                else:
+                    new_like.update_like_type()
+
+            elif 'saveComment' in request.form:
+                post_id = request.form['postId']
+                comment = request.form['comment']
+                new_comment = PostComments(post_id=post_id, user_id=user_id, comment=comment, connection=connection)
+                saved_comment = new_comment.get_comment_by_user_id_and_post_id()
+                if saved_comment != comment:
+                    new_comment.save()
+
+            elif 'unlike' in request.form:
+                like = PostLike(plid=request.form['unlike'], connection=connection)
+                like.delete()
+
+            elif 'uncomment' in request.form:
+                comment = PostComments(pcid=request.form['uncomment'], connection=connection)
+                comment.delete()
 
         if user and not user2:
             query = """select users.id ,posts.id, link, name, surname,date,description from users,posts where username='"""+user+"""' and users.id=posts.userid order by date desc"""
 
             cursor.execute(query)
 
-            for im in cursor:
-                images.append(im)
+            cursor.execute(query)
+            temp = cursor.fetchall()
+            for post in temp:
+                likes = post_like.get_likes_by_post_id(post[1])
+                if likes:
+                    posts_likes.update({post[1]: likes})
+                else:
+                    posts_likes.update({post[1]: False})
+
+                comments = post_comment.get_comments_by_post_id(post[1])
+                if comments:
+                    posts_comments.update({post[1]: comments})
+                else:
+                    posts_comments.update({post[1]: False})
+                images.append(post)
 
         if user2:
             query = """select users.id ,posts.id, link, name, surname,date,description from users,posts where username='"""+user2+"""' and users.id=posts.userid order by date desc"""
 
             cursor.execute(query)
+            temp = cursor.fetchall()
+            for post in temp:
+                likes = post_like.get_likes_by_post_id(post[1])
+                if likes:
+                    posts_likes.update({post[1]: likes})
+                else:
+                    posts_likes.update({post[1]: False})
 
-            for im in cursor:
-                images.append(im)
+                comments=post_comment.get_comments_by_post_id(post[1])
+                if comments:
+                    posts_comments.update({post[1]: comments})
+                else:
+                    posts_comments.update({post[1]: False})
+                images.append(post)
+
 
         if user and not user2:
             query ="""select link,users.username from (select following,users.username from users join follow on users.id=follow.follower where username='"""+user+"""') F, profilepic,users where F.following=profilepic.userid and F.following=users.id ORDER BY users.username ASC"""
@@ -409,8 +529,10 @@ def profile_page(user=None, user2=None):
         if user and not user2:
             query="""select userid,link, username, name, surname,mail from profilepic,users where username='"""+user+"""' and users.id=profilepic.userid"""
             cursor.execute(query)
-            for us in cursor:
-                userr.append(us)
+            userr = cursor.fetchall()
+            if userr:
+                interest = Interest(connection=connection)
+                user_interests = interest.get_interest_by_user_id(userr[0][0])
 
         if user2:
             query ="""select link,users.username from (select following,users.username from users join follow on users.id=follow.follower where username='"""+user2+"""') F, profilepic,users where F.following=profilepic.userid and F.following=users.id ORDER BY users.username ASC"""
@@ -426,12 +548,15 @@ def profile_page(user=None, user2=None):
             for im in cursor:
                 allfollowerpic.append(im)
 
-
         if user2:
             query="""select userid,link, username, name, surname,mail from profilepic,users where username='"""+user2+"""' and users.id=profilepic.userid"""
             cursor.execute(query)
-            for us in cursor:
-                userr.append(us)
+            userr = cursor.fetchall()
+            if userr:
+                interest = Interest(connection=connection)
+                user_interests = interest.get_interest_by_user_id(userr[0][0])
+
+
 
         connection.commit()
     if request.method =='POST':
@@ -492,7 +617,7 @@ def profile_page(user=None, user2=None):
             return redirect(url_for('profile_page',user=username))
 
 
-    return render_template('profile.html',user=user, user2=user2,allfollowerpic=allfollowerpic,allfollowingpic=allfollowingpic, current_time=now.strftime("%Y-%m-%d %H:%M:%S"),images=images,userr=userr,kullanici=kullanici)
+    return render_template('profile.html',user=user, user2=user2,allfollowerpic=allfollowerpic,allfollowingpic=allfollowingpic, current_time=now.strftime("%Y-%m-%d %H:%M:%S"),images=images,userr=userr,kullanici=kullanici,postsLikes=posts_likes,postsComments=posts_comments,user_interests=user_interests)
 
 @app.route('/add_pic', methods = ['GET','POST'])
 def add_pic():
@@ -584,7 +709,6 @@ def page_not_found(e):
     return render_template('500.html'), 500
 #
 # #END of EKREM CIHAD CETIN's space
-
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
