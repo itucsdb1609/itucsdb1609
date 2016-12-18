bc14cea1bc9b0ae31d0ce750117657ce37a1021b
Ali Ercan <ercanal@itu.edu.tr>
2016-12-18 18:38:38 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,10 @@ from .postComments import PostComments
 from .interest import Interest
 from .posts import posts
 from .profilepic import ProfilePic
+from .city import City
+from .collage import Collage
+from .register import Register
+from .login import Login
+from .admin import Admin
 
 __all__ = ['PostLike','PostComments','Interest','posts','ProfilePic','City','Collage','Register','Login','Admin',]
